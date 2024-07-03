855c61bfd7b68aba3b2d3754e9ab0d40ed921654
Benicio <gustavo024dias@gmail.com>
2024-07-03 16:11:03 -0300

    first commit

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 
@@ -7,9 +8,14 @@ app = Flask(__name__)
 def index():
     args = request.args
 
-    return jsonify({
-        "args": args
-    })
+    code = args.get('code')
+
+    url = (f"https://oauth.bitrix.info/oauth/token/?client_id=local.6684443e2eb8f5.18028528&client_secret"
+           f"=5I0F06k4ZLXn9LV4pfcE4rUh24X2TRehm82taFsWKiExgKI075&code={code}&grant_type=authorization_code")
+
+    response = requests.request("GET", url)
+
+    return jsonify(response.json())
 
 
 if __name__ == "__main__":
