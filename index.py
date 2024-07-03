f7916ae827d6104cb162a356b45a41ce8e6a3871
Benicio <gustavo024dias@gmail.com>
2024-07-03 16:21:59 -0300

    routa refresh token

--- index.py
+++ index.py
@@ -18,5 +18,20 @@ def index():
     return jsonify(response.json())
 
 
+@app.route("/refresh")
+def refresh():
+    args = request.args
+
+    refresh_token = args.get('refresh_token')
+
+    url = (f"https://oauth.bitrix.info/oauth/token/?client_id=local.6684443e2eb8f5.18028528&client_secret"
+           f"=5I0F06k4ZLXn9LV4pfcE4rUh24X2TRehm82taFsWKiExgKI075&refresh"
+           f"_token={refresh_token}&grant_type=refresh_token")
+
+    response = requests.request("GET", url)
+
+    return jsonify(response.json())
+
+
 if __name__ == "__main__":
     app.run()
