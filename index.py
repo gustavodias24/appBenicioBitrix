d046b1041d69f5be39bfacef5add3055b0142432
gustavodias <58337273+gustavodias24@users.noreply.github.com>
2024-08-25 11:58:19 -0300

    Update index.py para virginizar

--- index.py
+++ index.py
@@ -3,13 +3,8 @@ import requests
 
 app = Flask(__name__)
 
-client_scret = "io29XDkBsjeRkh4U2zpG0G5jhPJJBNKpybA29VsXqtS2woIDov"
-client_id = "local.669bbb5f70bf42.85105579"
-
-
-@app.route("/", methods=["POST"])
-def my_handler():
-    return jsonify({"funfando": request.get_json()})
+client_scret = ""
+client_id = ""
 
 
 @app.route("/token")
@@ -41,11 +36,5 @@ def refresh():
     return jsonify(response.json())
 
 
-# @app.route('/myhandler', methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"])
-# def my_handler():
-#     metodo_chamado = request.method
-#     return jsonify({"funfando": "sim", "metodo_chamado": metodo_chamado})
-
-
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=SUA_PORTA)
